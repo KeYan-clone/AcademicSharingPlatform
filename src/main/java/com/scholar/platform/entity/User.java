405afdbcda462b22098a35d42792eec9e9aa87a0
keyan <keyanli259@gmail.com>
2025-12-27 22:49:19 +0800

    fix：添加user的scholar字段

--- src/main/java/com/scholar/platform/entity/User.java
+++ src/main/java/com/scholar/platform/entity/User.java
@@ -5,6 +5,7 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.hibernate.annotations.UuidGenerator;
+import org.springframework.data.elasticsearch.annotations.Field;
 
 import java.time.LocalDateTime;
 
@@ -39,6 +40,10 @@ public class User {
 
   @Column(name = "preferences", columnDefinition = "TEXT")
   private String preferences;
+
+  @OneToOne(mappedBy = "user", cascade = CascadeType.ALL, fetch = FetchType.LAZY, optional = true)
+  private Scholar scholar;
+
 
   public enum UserRole {
     USER, ADMIN
