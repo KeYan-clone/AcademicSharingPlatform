41bed65bd6040ceddfd982d6eb2427688187894b
keyan <keyanli259@gmail.com>
2025-12-27 14:15:08 +0800

    feat:添加允许@CrossOrigin跨域

--- src/main/java/com/scholar/platform/controller/AuthController.java
+++ src/main/java/com/scholar/platform/controller/AuthController.java
@@ -16,6 +16,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+@CrossOrigin
 @RestController
 @RequestMapping("/auth")
 @RequiredArgsConstructor
